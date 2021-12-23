4a90b29e13836833dc508fcb2d16be0778524193
Jon Evans <jon@rezendi.com>
2021-12-23 15:57:23 -0800

    successfully encrypting the code before it's run

--- contracts/CodeCaller.sol
+++ contracts/CodeCaller.sol
@@ -7,6 +7,7 @@ contract CodeCaller {
     string private greeting;
     address private subContractAddress;
     string private subContractAbiSignature;
+    bytes private encryptedCode;
 
     constructor(string memory _greeting) {
         console.log("Deploying a Greeter with greeting:", _greeting);
@@ -17,15 +18,22 @@ contract CodeCaller {
         return greeting;
     }
 
+    function setEncryptedCode(bytes memory _code) public {
+        console.log("setting encrypted code, lengtH:", _code.length);
+        encryptedCode = _code;
+    }
+
     function create(bytes memory code) internal returns (address addr){
         assembly {
             addr := create(0,add(code,0x20), mload(code))
         }
     }
-    function createSubContract(bytes memory _code, string memory _abiSignature) public {
+
+    function createSubContract(string memory _abiSignature, bytes memory _key) public {
+        console.log("Creating subcontract", _abiSignature);
         subContractAbiSignature = _abiSignature;
-        console.log("Creating subcontract");
-        subContractAddress = create(_code);
+        bytes memory subContractCode = encryptDecrypt(encryptedCode, _key);
+        subContractAddress = create(subContractCode);
         console.log("created", subContractAddress);
     }
 
@@ -35,5 +43,27 @@ contract CodeCaller {
         console.log("call success", success);
         greeting = abi.decode(data, (string));
         console.log("new greeting", greeting);
+    }
+
+    function encryptDecrypt (bytes memory data, bytes memory key) public pure returns (bytes memory result) {
+        uint256 length = data.length;
+
+        assembly {
+            result := mload (0x40)
+            mstore (0x40, add (add (result, length), 32))
+            mstore (result, length)
+        }
+
+        for (uint i = 0; i < length; i += 32) {
+            bytes32 hash = keccak256 (abi.encodePacked (key, i));
+            bytes32 chunk;
+            assembly {
+                chunk := mload (add (data, add (i, 32)))
+            }
+            chunk ^= hash;
+            assembly {
+                mstore (add (result, add (i, 32)), chunk)
+            }
+        }
     }
 }
